5c2cb1bbc7a6e2a02bd82c52b0b9d02633f39d89
Elborn666 <elborn18@gmail.com>
2023-09-03 08:47:30 +0300

    зробив регістрацію столу та підписку

--- js/reservation.js
+++ js/reservation.js
@@ -1,7 +1,7 @@
-const backdrop = document.querySelector('.backdrop__subscribe')
+const backdrop = document.querySelector('.backdrop')
 const body = document.querySelector('body')
-const subscribeForm = document.querySelector('.contact__form')
-const buttonCloseModal = document.querySelector('.modal__button-subscribe')
+const subscribeForm = document.querySelector('.reserv__form')
+const buttonCloseModal = document.querySelector('.modal-button')
 
 
 buttonCloseModal.addEventListener('click', onBtnClose)
