a6bdbf67c1648896f5edc1371e4e90e35408b4be
Elborn666 <elborn18@gmail.com>
2023-09-03 09:29:57 +0300

    зробив скрол

--- js/scroll-to.js
+++ js/scroll-to.js
@@ -6,8 +6,8 @@ function scrollTo(element) {
     })
 }
 
-let aboutRefs = document.querySelector('.js-about')
-let aboutTop = document.querySelector('.about')
+let aboutRefs = document.querySelector('.js-plan')
+let aboutTop = document.querySelector('.plan')
 
 aboutRefs.addEventListener('click', () => {
     scrollTo(aboutTop);
@@ -15,55 +15,3 @@ aboutRefs.addEventListener('click', () => {
 })
 
 //====================================================================================
-
-function scrollTo(element) {
-    window.scroll({
-        left: 0,
-        top: element.offsetTop,
-        behavior: 'smooth'
-    })
-}
-
-let servicesRef = document.querySelector('.js-services')
-let servicesTop = document.querySelector('.price')
-
-servicesRef.addEventListener('click', () => {
-    scrollTo(servicesTop);
-    console.log('Click for button')
-})
-
-//====================================================================================
-
-function scrollTo(element) {
-    window.scroll({
-        left: 0,
-        top: element.offsetTop,
-        behavior: 'smooth'
-    })
-}
-
-let barbersRef = document.querySelector('.js-barbers')
-let barbersTop = document.querySelector('.team')
-
-barbersRef.addEventListener('click', () => {
-    scrollTo(barbersTop);
-    console.log('Click for button')
-})
-
-//=================================================================================
-
-function scrollTo(element) {
-    window.scroll({
-        left: 0,
-        top: element.offsetTop,
-        behavior: 'smooth'
-    })
-}
-
-let contactsRef = document.querySelector('.js-contacts')
-let contactsTop = document.querySelector('.contact')
-
-contactsRef.addEventListener('click', () => {
-    scrollTo(contactsTop);
-    console.log('Click for button')
-})
